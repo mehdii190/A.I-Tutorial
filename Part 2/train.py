89b7fe699060d06efbf7998df251205b5d33c461
main <mehdi190gym@gmail.com>
2022-10-10 22:45:41 +0330

    add train

--- Part 2/train.py
+++ Part 2/train.py
@@ -5,3 +5,12 @@ import pandas as pd
 a=np.random.randint(5,10 ,size=5)
 b=pd.Series(a)
 print(b)
+
+
+########################################
+
+a = np.random.randint(5,10)
+b= np.random.randint(1,100 ,size= a)
+c= np.array(b)
+f = pd.Series(c)
+print(f)
