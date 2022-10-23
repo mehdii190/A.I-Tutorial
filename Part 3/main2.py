ef845e2abcdc43e251570af09a9b5c875bf8e3a3
mehdii190 <mehdi190gym@gmail.com>
2022-10-23 19:44:20 +0330

    scatter

--- Part 3/main2.py
+++ Part 3/main2.py
@@ -2,11 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#years=[1960,1970,1980,1990,2000,2010,2016]
+#iran_top=[21.91,32.2,34,54,66,78,89]
+#iraq_top=[21.91,32.2,34,50,60,77,86]
+#plt.xlabel('Year')
+#plt.ylabel('Populustion')
+#plt.plot(years,iran_top,ls='-',marker='o',mew=4)
+#plt.plot(years,iraq_top,ls='-',lw=1)
+
+#plt.legend(['iran','iraq'],loc='best')
+#plt.grid()
+#plt.show()
+
+
+
+#####################################
+
+
+
 years=[1960,1970,1980,1990,2000,2010,2016]
 iran_top=[21.91,32.2,34,54,66,78,89]
-iraq_top=[21.91,32.2,34,50,60,77,86]
-plt.xlabel('Year')
-plt.ylabel('Populustion')
-plt.plot(years,iran_top,ls='-',marker='o',mew=4)
-plt.plot(years,iraq_top,ls='-',lw=1)
+plt.scatter(years,iran_top,s=40)
 plt.show()
