f3da472f8c72d33394eb1446969d79439bda5762
main <mehdi190gym@gmail.com>
2022-10-23 17:20:49 +0330

    add train more

--- Part 2/train7.py
+++ Part 2/train7.py
@@ -1,5 +1,22 @@
 import numpy as np
 
-
-def enter(m,n):
-    pass
+#numlist = []
+def enter():
+    numlist = []
+    while True:
+        list = []
+        while True:
+            numbers = input('enter your num: end')
+            if numbers == 'end':
+                break
+            list.append(numbers)
+        numlist.append(list)
+        choice = input('enter your choice: ')
+        if choice == 'end':
+            break
+    a = np.array(numlist,dtype=int)
+    print(a)
+    b = a.shape
+    c = np.zeros(b)
+    print(c)
+enter()
