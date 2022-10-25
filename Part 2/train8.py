346ab79b24b8509b3309a2fc3e60d70bc8ea035f
main <mehdi190gym@gmail.com>
2022-10-25 03:52:06 +0330

    add train more

--- Part 2/train8.py
+++ Part 2/train8.py
@@ -1,4 +1,10 @@
 import numpy as np
 
 
-array = np.random.uniform(1, 10, (4, 4))
+array = np.random.uniform(1,10,(4,4))
+ar = np.array(array)
+print(array)
+print("###################")
+arr = ar[:2, 2:], ar[1:2, :2]
+neg = np.negative(arr)
+print(neg)
