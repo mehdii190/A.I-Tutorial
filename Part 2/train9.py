154a78fdfac095e451a431876040c45bb995ffa3
main <mehdi190gym@gmail.com>
2022-10-25 04:26:18 +0330

    add train more

---
+++ Part 2/train9.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+print(df)
+
+print("Number of attempts in the examination is greater than 2:")
+
+df2=df.loc[:,'attempts']
+for i in df2:
+    if i > 2:
+        print(i)
