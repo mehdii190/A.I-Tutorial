bbd9c33de94685f3d9693b063e81d715f562446e
main <mehdi190gym@gmail.com>
2022-11-25 21:44:18 +0330

    add train

---
+++ Part 6/train.py
@@ -0,0 +1,21 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+iris = datasets.load_iris()
+
+x_train , x_test , y_train , y_test = train_test_split(iris.data, iris.target, test_size=0.3,
+                                                       random_state=42,stratify=iris.target)
+
+
+knn = KNeighborsClassifier(5)
+knn.fit(x_train, y_train)
+y_predict = knn.predict(x_test)
+
+print(y_predict)
+print("################")
+for i in y_predict:
+    if not i:
+        print(i,sep="\n")
