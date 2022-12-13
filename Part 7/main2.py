0d6d38c0f7ebe1f007907dd4db5ed48b918f1e33
mehdii190 <mehdi190gym@gmail.com>
2022-12-13 19:53:06 +0330

    more

--- Part 7/main2.py
+++ Part 7/main2.py
@@ -2,6 +2,8 @@ from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+import sklearn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -34,7 +36,7 @@ print(reg.score(x, y))
 #print(reg.score(x_train, y_train))
 
 
-
+############
 
 plt.scatter(y_test,y_pred)
 plt.plot()
@@ -42,14 +44,33 @@ plt.xlabel("prices")
 plt.ylabel("predicted prices")
 plt.show()
 
+############ 
 
 
+mse =mean_squared_error(y_test, y_pred)
+print("**** MSE: ",mse,"****")
 
 
+#############
 
 
+#new_x = boston.data[:,[1,2]]
+#new_y = boston.target
+
+#new_x_train, new_y_train, new_x_test, new_y_test =train_test_split(new_x,new_y,test_size=0.3,random_state=42)
+#new_reg = LinearRegression()
+#new_reg.fit(new_x_train,new_y_train)
+#new_x_predict = new_reg.predict(new_x_test)
 
 
+############################################
+
+
+reg = LinearRegression()
+#cv_scores = cross_val_score(reg, x , y, scoring="r2",cv=5)
+cv_scores2 = cross_val_score(reg, x,y, scoring="neg_mean_squared_error",cv=5)
+print(cv_scores2)
+print(np.mean(cv_scores2))
 
 
 
