6e9f16e2cacd2c6ccb197002f2b600d5ee5ed7e8
main <mehdi190gym@gmail.com>
2022-12-19 00:37:25 +0330

    exam 1

---
+++ exam/exam1.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+
+sl1 = pd.read_csv("I:/video I.A/exam/salary_data_first.csv")
+sl2 = pd.read_csv("I:/video I.A/exam/salary_data_second.csv")
+
+
+sl1.drop("pre",axis=1,inplace=True)
+sl2.drop("pre",axis=1,inplace=True)
+
+
+##########################################
+
+sl_df = pd.concat([sl1,sl2],axis=0,ignore_index=True)
+#################################
+
+sl_df.replace('?',np.nan,inplace=True)
+new_df = sl_df.dropna()
+new_df.reset_index(drop=True,inplace=True)
+new_df.fillna(0,inplace=True)
+new_df = new_df.astype(float)
+#################################
+
+x = new_df.iloc[:, :-1].values
+y = new_df.iloc[:, -1].values
+
+
+#################################
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
+
+reg = LinearRegression()
+
+reg.fit(x_train,y_train)
+
+y_pred =reg.predict(x_test)
+################################
+
+print("R2 Reg: ", reg.score(x_test, y_test))
+
+##################################
+
+plt.scatter(x_test, y_test, c="b")
+plt.scatter(x_train, y_train, c="r")
+plt.plot(x_test,y_pred, c="brown")
+plt.title("linear reg")
+plt.xlabel("Years experience")
+plt.ylabel("Salary")
+plt.show()
+
+
+###############################
+
+mse =mean_squared_error(y_test, y_pred)
+print("mse===> ", mse ," <===##" )
+
+################################
+
+df_scores = cross_val_score(reg, x , y, scoring="r2",cv=5)
+print(df_scores)
+print(np.mean(df_scores))
+
+
+
+
+
