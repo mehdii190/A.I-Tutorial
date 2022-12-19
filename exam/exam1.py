75eb0a29bc87a7db6b048a5410e3b0fe9e158244
main <mehdi190gym@gmail.com>
2022-12-19 19:31:28 +0330

    new exam 1

--- exam/exam1.py
+++ exam/exam1.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
+import seaborn as sb
 
 sl1 = pd.read_csv("I:/video I.A/exam/salary_data_first.csv")
 sl2 = pd.read_csv("I:/video I.A/exam/salary_data_second.csv")
@@ -52,7 +53,9 @@ plt.title("linear reg")
 plt.xlabel("Years experience")
 plt.ylabel("Salary")
 plt.show()
-
+###############
+#new_df.boxplot()
+#plt.show()
 
 ###############################
 
@@ -62,8 +65,51 @@ print("mse===> ", mse ," <===##" )
 ################################
 
 df_scores = cross_val_score(reg, x , y, scoring="r2",cv=5)
-print(df_scores)
-print(np.mean(df_scores))
+print("df score: ",df_scores)
+print("mean score: ",np.mean(df_scores))
+
+
+###############################
+
+new_df.boxplot(column=["Salary"])
+plt.show()
+
+print(new_df['Salary'].describe())
+
+
+uppper_boundary=new_df['Salary'].mean() + 3* new_df['Salary'].std()
+lower_boundary=new_df['Salary'].mean() - 3* new_df['Salary'].std()
+print("lower : ",lower_boundary), print("upper : ",uppper_boundary),print("mean : ",new_df['Salary'].mean())
+
+################################
+
+new_df.boxplot(column=["YearsExperience"])
+plt.show()
+
+print(new_df['YearsExperience'].describe())
+
+
+uppper_boundary=new_df['YearsExperience'].mean() + 3* new_df['YearsExperience'].std()
+lower_boundary=new_df['YearsExperience'].mean() - 3* new_df['YearsExperience'].std()
+print("lower : ",lower_boundary), print("upper : ",uppper_boundary),print("mean : ",new_df['YearsExperience'].mean())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
